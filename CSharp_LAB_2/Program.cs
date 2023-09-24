bdf87773335d2c2a27eadacf0d36f8e5c88f078e
march <marchenkopavel2006@gmail.com>
2023-09-25 00:58:12 +0200

    Added Task № 149 Changed GenerateRandomArray

--- CSharp_LAB_2/Program.cs
+++ CSharp_LAB_2/Program.cs
@@ -8,7 +8,7 @@ namespace CSharp_LAB_2
     {
         public static void Main(string[] args)
         {
-            Task130();
+         Task149();
         }
 
         /// <summary>
@@ -17,8 +17,10 @@ namespace CSharp_LAB_2
         /// <param name="minValue">Минимальное значение (по умолчанию 1).</param>
         /// <param name="maxValue">Максимальное значение (по умолчанию 100).</param>
         /// <param name="length">Длина массива (по умолчанию 10).</param>
+        /// <param name="allowDuplicates">Разрешить повторение элементов (по умолчанию true)</param>
         /// <returns>Случайный массив целых чисел.</returns>
-        static int[] GenerateRandomArray(int minValue = 1, int maxValue = 100, int length = 10)
+        static int[] GenerateRandomArray(int minValue = 1, int maxValue = 100, int length = 10,
+            bool allowDuplicates = true)
         {
             Random random = new();
             //check input valu
@@ -33,14 +35,25 @@ namespace CSharp_LAB_2
             }
 
             int[] array = new int[length];
-            Console.Write("Array: [ ");
             for (int i = 0; i < length; i++)
             {
-                array[i] = random.Next(minValue, maxValue + 1);
-                Console.Write(array[i] + " ");
+                if (allowDuplicates)
+                {
+                    array[i] = random.Next(minValue, maxValue + 1);
+                }
+                else //check repeating elements
+                {
+                    int value;
+                    do
+                    {
+                        value = random.Next(minValue, maxValue + 1);
+                    } while (Array.IndexOf(array, value, 0, i) != -1);
+
+                    array[i] = value;
+                }
             }
 
-            Console.WriteLine("]");
+            Console.WriteLine("Array : [" + string.Join(", ", array) + "]");
 
             return array;
         }
@@ -83,7 +96,7 @@ namespace CSharp_LAB_2
                 Console.WriteLine($"No more numbers {M} in the sequence.");
             }
         }
-        
+
         /// <summary>
         ///  130 : Даны целые положительные числа а1, а2, ..., an.
         ///        Найти среди них те, которые являются квадратами некоторого числа m.
@@ -104,6 +117,7 @@ namespace CSharp_LAB_2
                 Console.WriteLine("There are no numbers in the array that are squares of some number m.");
             }
         }
+
         ///<summary>
         ///принимает массив целых положительных чисел numbers и находит числа, являющиеся квадратами
         ///</summary>
@@ -127,6 +141,30 @@ namespace CSharp_LAB_2
             Array.Copy(squares, result, count);
 
             return result;
+        }
+
+        /// <summary>
+        ///  149: Даны две последовательности а1, а2, ..., an и b1, b2, ..., bm (m < n).
+        ///       В каждой из них члены различны.
+        ///       Верно ли, что все члены второй последовательности входят в первую последовательность?
+        /// </summary>
+        static void Task149()
+        {
+            Console.Write("1 ");
+            int[] sequenceA = GenerateRandomArray(1, 15, 15, false);
+            Console.Write("2 ");
+            int[] sequenceB = GenerateRandomArray(1, 10, 5, false); //(m < n)
+
+            bool allInSequenceA = sequenceB.All(x => sequenceA.Contains(x));
+
+            if (allInSequenceA)
+            {
+                Console.WriteLine("All members of the second sequence enter the first sequence.");
+            }
+            else
+            {
+                Console.WriteLine("Not all members of the second sequence are in the first sequence.");
+            }
         }
     }
 }
